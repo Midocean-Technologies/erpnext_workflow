86116e3576024f171e42cc9e0006d875cdfdd86c
vaishali <vaishali>
2025-11-24 16:17:46 +0530

    remove workflow action override file

--- erpnext_workflow/__init__.py
+++ erpnext_workflow/__init__.py
@@ -1,6 +1,1 @@
 __version__ = "0.0.1"
-
-from frappe.workflow.doctype.workflow_action import workflow_action
-from erpnext_workflow.erpnext_workflow.overrides import override_workflow_action
-
-workflow_action.process_workflow_actions = override_workflow_action.process_workflow_actions
