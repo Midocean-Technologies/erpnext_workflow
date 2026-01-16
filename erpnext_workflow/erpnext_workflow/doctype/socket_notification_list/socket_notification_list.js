9feec8243fe590e8e88955fa6249335f0f3012aa
Anju1207 <sudeshkakran999@gmail.com>
2026-01-16 15:39:10 +0530

    FCM Notification for Workflow Action and Comment

--- erpnext_workflow/erpnext_workflow/doctype/socket_notification_list/socket_notification_list.js
+++ erpnext_workflow/erpnext_workflow/doctype/socket_notification_list/socket_notification_list.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2025, Midocean Technologies Pvt Ltd and contributors
+// Copyright (c) 2026, Midocean Technologies Pvt Ltd and contributors
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Socket Notification List", {
--- erpnext_workflow/erpnext_workflow/doctype/socket_notification_list/socket_notification_list.py
+++ erpnext_workflow/erpnext_workflow/doctype/socket_notification_list/socket_notification_list.py
@@ -1,9 +1,25 @@
-# Copyright (c) 2025, Midocean Technologies Pvt Ltd and contributors
-# For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from erpnext_workflow.mobile_api.fcm_notification import triggerd_fcm_notification
 
 class SocketNotificationList(Document):
-	pass
+	def after_insert(self):
+		frappe.enqueue("erpnext_workflow.erpnext_workflow.doctype.socket_notification_list.socket_notification_list.send_fcm_notification",doc=self, queue='long')
+
+@frappe.whitelist()
+def send_fcm_notification(doc):
+	try:
+		if doc.user:
+			user_fcm_token = frappe.get_value("User", doc.user, 'user_fcm_token')
+			if user_fcm_token:
+				if doc.notification_from == "Comment":
+					title = 'Comment From {0}'.format(doc.comment_by)
+				else:
+					title = 'Workflow Action Requered !'
+				triggerd_fcm_notification(user_fcm_token, title, doc.message, doc.json)
+
+	except Exception as e:
+		frappe.log_error("FCM Notification Error", frappe.get_traceback(e))
+
+
